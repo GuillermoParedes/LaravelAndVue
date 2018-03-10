9f97596499bac4e9914d047b4db5d5ca906a176b
CHIGY <gdavid.ptorrez@gmail.com>
2018-03-09 20:13:18 -0400

    Template VUE

---
+++ laravelandvue/resources/assets/js/components/Comments.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="comments">
+    <div class="row">
+      <div class="col-md-5">
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <span class="glyphicon glyphicon-comment"></span> Chat
+          </div>
+          <div class="panel-body">
+            <ul class="chat" id="chatWindow">
+              
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
